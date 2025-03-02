d5b5ed075bc6c997f9e50b2589335df6484ded90
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-03-02 10:30:05 +0000

    Refactor data file paths and add new client and prediction scripts

--- src/client.py
+++ src/client.py
@@ -3,10 +3,13 @@ from openai import OpenAI
 # client = OpenAI(
 #     api_key="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")
 
+# dont worry about the api key, it's a public key
+# controlled by @binaryyuki's server and it's not a secret key
 client = OpenAI(
     api_key="sk-U7WAlocyRe8nagQcqG1uXzRUKyU8PnUc4OHY6rGU8fENVR8F",
     base_url="https://ai.tzpro.xyz/v1"
 )
+
 
 def get_client():
     """
