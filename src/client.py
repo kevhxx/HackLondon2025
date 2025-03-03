4992e17cc7af74794e31664f8d7adf99fb6555bb
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-03-03 19:59:35 +0000

    Load environment variables for OpenAI API configuration

--- src/client.py
+++ src/client.py
@@ -1,13 +1,18 @@
-from openai import OpenAI
+from logging import raiseExceptions
 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # client = OpenAI(
 #     api_key="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")
 
 # dont worry about the api key, it's a public key
 # controlled by @binaryyuki's server and it's not a secret key
 client = OpenAI(
-    api_key="sk-U7WAlocyRe8nagQcqG1uXzRUKyU8PnUc4OHY6rGU8fENVR8F",
-    base_url="https://ai.tzpro.xyz/v1"
+    api_key=os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") else print("OPENAI_API_KEY not found"),
+    base_url=os.getenv("OPENAI_BASE_URL") if os.getenv("OPENAI_BASE_URL") else print("OPENAI_BASE_URL not found")
 )
 
 
